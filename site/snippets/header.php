53f48102074dfd82b5e2eb992829deeed6922874
Tristan <tristan.bagot@gmail.com>
2016-09-01 17:15:31 +0200

    clean header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -8,38 +8,42 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<link rel="canonical" href="<?php echo html($page->url()) ?>" />
 	<?php if($page->isHomepage()): ?>
-      <title><?php echo $site->title()->html() ?></title>
-    <?php else: ?>
-      <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
-    <?php endif ?>
-	<?php if($page->isHomepage()): ?>
-		<meta name="description" content="<?php echo $site->description()->html() ?>">
+		<title><?= $site->title()->html() ?></title>
 	<?php else: ?>
-		<?php if(!$page->description()->empty()): ?>
-			<meta name="description" content="<?php echo $page->description()->excerpt(250) ?>">
+		<title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
+	<?php endif ?>
+	<?php if($page->isHomepage()): ?>
+		<meta name="description" content="<?= $site->description()->html() ?>">
+	<?php else: ?>
+		<meta name="DC.Title" content="<?= $page->title()->html() ?>" />
+		<?php if(!$page->text()->empty()): ?>
+			<meta name="description" content="<?= $page->text()->excerpt(250) ?>">
+			<meta name="DC.Description" content="<?= $page->text()->excerpt(250) ?>"/ >
+			<meta property="og:description" content="<?= $page->text()->excerpt(250) ?>" />
 		<?php endif ?>
 	<?php endif ?>
 	<meta name="robots" content="index,follow" />
-	<meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-	<meta name="DC.Title" content="<?php echo $page->title()->html() ?>" />
-    <meta name="DC.Description" content="<?php echo $page->description()->html() ?>"/ >
-    <?php if($page->isHomepage()): ?>
-      <meta property="og:title" content="<?php echo $site->title()->html() ?>" />
-    <?php else: ?>
-      <meta property="og:title" content="<?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?>" />
-    <?php endif ?>
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="<?php echo html($page->url()) ?>" />
-    <?php if(!$site->ogimage()->empty()): ?>
-	<meta property="og:image" content="<?= $site->ogimage()->toFile()->width(1200)->url() ?>"/>
+	<meta name="keywords" content="<?= $site->keywords()->html() ?>">
+	<?php if($page->isHomepage()): ?>
+		<meta itemprop="name" content="<?= $site->title()->html() ?>">
+		<meta property="og:title" content="<?= $site->title()->html() ?>" />
+	<?php else: ?>
+		<meta property="og:title" content="<?= $site->title()->html() ?> | <?php echo $page->title()->html() ?>" />
+		<meta itemprop="name" content="<?= $site->title()->html() ?> | <?php echo $page->title()->html() ?>">
 	<?php endif ?>
-    <meta property="og:description" content="<?php echo $page->description()->html() ?>" />
-    <?php if($page->isHomepage()): ?>
-      <meta itemprop="name" content="<?php echo $site->title()->html() ?>">
-    <?php else: ?>
-      <meta itemprop="name" content="<?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?>">
-    <?php endif ?>
-    <meta itemprop="description" content="<?php echo $site->description()->html() ?>">
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="<?= html($page->url()) ?>" />
+	<?php if($page->content()->name() == "project"): ?>
+		<?php if (!$page->featured()->empty()): ?>
+			<meta property="og:image" content="<?= resizeOnDemand($page->image($page->featured()), 1200) ?>"/>
+		<?php endif ?>
+	<?php else: ?>
+		<?php if(!$site->ogimage()->empty()): ?>
+			<meta property="og:image" content="<?= $site->ogimage()->toFile()->width(1200)->url() ?>"/>
+		<?php endif ?>
+	<?php endif ?>
+
+	<meta itemprop="description" content="<?= $site->description()->html() ?>">
 	<link rel="shortcut icon" href="<?= url('assets/images/favicon.ico') ?>">
 	<link rel="icon" href="<?= url('assets/images/favicon.ico') ?>" type="image/x-icon">
 
