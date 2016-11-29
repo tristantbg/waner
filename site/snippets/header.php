f177b2ce46539e8c9e24eaab6673e389e31b8e0c
Tristan <tristan.bagot@gmail.com>
2016-11-29 13:15:01 +0100

    updated vendors

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -56,7 +56,7 @@
 	echo js('assets/js/vendor/modernizr.min.js');
 	?>
 	
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="<?= url('assets/js/vendor/jquery.min.js') ?>">\x3C/script>')</script>
 
 	<?php if(!$site->customcss()->empty()): ?>
